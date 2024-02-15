0cb2dc73c8c0b704c6e2f4ce644af2b091504e63
JWarconsin <133745642+JWarconsin@users.noreply.github.com>
2024-02-15 15:53:55 +0100

    3

--- EvalReact/src/components/Affiche.jsx
+++ EvalReact/src/components/Affiche.jsx
@@ -4,23 +4,23 @@ export function Affiche(props) {
     let data = props.data;
 
     return (
-        <article>
+        <article key={props.index} >            
             <div className="card">
                 <div className="banner">
                     <div className="content">
-                        <img src="../assets/bg.png" alt="" />
+                        <img src={"https://image.tmdb.org/t/p/w500"+props.data.poster_path} alt="" />
                         <div className="right">
                             <div className="top">
-                                <div className="score">4/5</div>
+                                <div className="score">{props.data.vote_average}/10</div>
                                 <div className="title-date">
-                                    <h1>TOTO ([2022])</h1>
+                                    <h1>{props.data.original_title} ([{props.data.release_date}])</h1>
                                     <span> Science-fi - 2h29</span>
                                 </div>
                             </div>
                             <div className="synopsis">
                                 <h2>Synopsis</h2>
                                 <p>
-                                    Film pas ouf mais bon c'est pas grave
+                                    {props.data.overview}
                                 </p>
                             </div>
                         </div>
@@ -33,6 +33,5 @@ export function Affiche(props) {
                 </div>
             </div>
         </article>
-
     );
 }
